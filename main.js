dfb5e855e0d34f71f108ff894100a0320bad4aa4
Marco Petrini <marcopetrini1994@gmail.com>
2020-04-16 14:44:49 +0200

    step 2 - stampa in pagina nomi e cognomi degli studenti

--- main.js
+++ main.js
@@ -12,8 +12,58 @@ var student = {
   eta: 26
 }
 
+ $('body').append('<h2>Dati primo studente</h2>');
+
 // ciclo for-in per accedere ai valori di tutte le proprietà dell'oggetto
 for (var key in student) {
-  $('body').append('<h2>' + key + ': ' + student[key] + '</h2>');
-  console.log(student[key]);
+  $('body').append('<h3>' + key + ': ' + student[key] + '</h3>');
+
+}
+
+// creo un array di studenti
+var students = [
+  {
+    nome: 'Marco',
+    cognome: 'Petrini',
+    eta: 26
+  },
+  {
+    nome: 'Paolo',
+    cognome: 'Fiaschi',
+    eta: 30
+  },
+  {
+    nome: 'Alessandro',
+    cognome: 'Matonti',
+    eta: 36
+  },
+  {
+    nome: 'Francesco',
+    cognome: 'Ferruccio',
+    eta: 28
+  },
+  {
+    nome: 'Federica',
+    cognome: 'Tosti',
+    eta: 34
+  },
+  {
+    nome: 'Antonella',
+    cognome: 'Cino',
+    eta: 33
+  }
+
+];
+
+// scrivo titolo in pagina
+$('body').append('<h2>Nomi e cognomi degli studenti</h2>')
+
+// ciclo per stampare in pagina i nomi e i cognomi degli studenti
+for (var i = 0; i < students.length; i++) {
+
+  var nomeStudente = students[i].nome;
+  var cognomeStudente = students[i].cognome;
+
+  $('body').append('<h4>' + 'Nome: ' + nomeStudente + '</h4>' + '<h4>' + 'Cognome: ' + cognomeStudente + '</h4>');
+
 }
